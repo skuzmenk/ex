d7e12bbf2bc3ea737369a587636101176bc1e6be
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2024-11-18 10:19:38 +0200

    Add project files.

---
+++ ex/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            try
+            {
+                for (int i = 10; i < 30; i++)
+                {
+                    StreamReader sr = new StreamReader($"{i}.txt");
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
