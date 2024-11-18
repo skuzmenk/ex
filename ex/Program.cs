c643f059cf54c50f513f95fe7c73a70e06e76bc8
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2024-11-18 10:44:00 +0200

    add exceptions & files

--- ex/Program.cs
+++ ex/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace ex
@@ -11,25 +7,49 @@ namespace ex
     {
         static void Main(string[] args)
         {
-            String line;
+            int[] arr = new int[40];
+            int j = 0;
             try
             {
                 for (int i = 10; i < 30; i++)
                 {
-                    StreamReader sr = new StreamReader($"{i}.txt");
-                    line = sr.ReadLine();
-                    while (line != null)
+                    try
                     {
-                        Console.WriteLine(line);
-                        line = sr.ReadLine();
+                        using (StreamReader sr = new StreamReader($"{i}.txt"))
+                        {
+                            string line = sr.ReadLine();
+                            string line1 = sr.ReadLine();
+                            if (string.IsNullOrEmpty(line))
+                            {
+                                throw new InvalidDataException($"File {i}.txt contains no valid data.");
+                            }
+
+                            arr[j] = int.Parse(line);
+                            arr[j+1] = int.Parse(line1);
+                            Console.WriteLine($"File {i}.txt: {arr[j]}");
+                            Console.WriteLine($"File {i}.txt: {arr[j+1]}");
+                        }
                     }
-                    sr.Close();
+                    catch (FileNotFoundException)
+                    {
+                        Console.WriteLine($"File {i}.txt does not exist.");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine($"File {i}.txt contains invalid data.");
+                    }
+                    catch (InvalidDataException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    j += 2;
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("Exception: " + e.Message);
+                Console.WriteLine("Unexpected exception: " + e.Message);
             }
+
             Console.ReadLine();
         }
     }
