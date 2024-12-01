430c73f1c613ed3f9a0e8f8aa87e549ebfaa2f93
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2024-12-01 22:03:09 +0200

    Add searching files and regex

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace ex2
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            Regex regexExtForImage = new Regex(@"^\.((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
+            foreach(string file in files) 
+            {
+                if(regexExtForImage.IsMatch(files[0]))
+                {
+
+                }
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
